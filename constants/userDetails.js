a9344084bf2ec4e4107acbc18b91ef9eb71688fe
Biswajit Das <bd.partha@gmail.com>
2022-02-19 22:17:51 +0600

    Project Initiated

---
+++ constants/userDetails.js
@@ -0,0 +1,36 @@
+const users = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    email: "john7@base2brand.com",
+    password: "123456",
+    address: "123 Main St",
+    city: "Houston",
+    state: "Texas",
+    zip: "15680",
+    country: "United States",
+    phone: "1234567890",
+    alias: "123 Main St",
+    birthDay: "1",
+    birthMonth: "February",
+    birthYear: "1990",
+  },
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    email: "jane7@base2brand.com",
+    password: "123456",
+    address: "123 Main St",
+    city: "Houston",
+    state: "Texas",
+    zip: "15680",
+    country: "United States",
+    phone: "1234567890",
+    alias: "123 Main St",
+    birthDay: "2",
+    birthMonth: "April",
+    birthYear: "1998",
+  },
+];
+
+export default users;
